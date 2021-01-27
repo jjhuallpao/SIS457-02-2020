7fcbbe1d4b915f6922be3a8ed9792d95cdca3fec
Luis Antonio Molina Yampa <antoniomolina.yampa@gmail.com>
2021-01-27 09:07:36 -0400

    Codigo Base 2do parcial

---
+++ 2doParcialSIS457.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
+
+void agregar_al_vector(list<int> &A,int x)
+{
+	//Su codigo aqui
+}
+
+/*sort the vector A in ascending order*/
+void ordenar_vector_asc(list<int> &A)
+{
+	//Your code here
+}
+
+/*reverses the vector A*/
+void reversa_vector(list<int> &A)
+{
+	//Your code here
+}
+
+/*returns the size of the vector  A */
+int long_de_vector(list<int> &A)
+{
+	//Your code here
+	return 0;
+}
+
+/*sorts the vector A in descending order*/
+void ordenar_vector_desc(list<int> &A)
+{
+	//Your code here
+}
+
+/*prints space separated 
+elements of vector A*/
+void imprimir_vector(list<int> &A)
+{
+	//Your code here
+}
+
+int main()
+{
+	
+	
+	return 0;
+}
+
